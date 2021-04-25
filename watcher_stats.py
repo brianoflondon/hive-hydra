7f645603234b28d90f3af49c39212a44542b38d2
brianoflondon <bol@snnsite.com>
2021-04-25 13:13:38 +0000

    added watcher_stats which counts stuff

--- watcher_stats.py
+++ watcher_stats.py
@@ -2,6 +2,7 @@ from beem import Hive
 from beem.account import Account
 from beem.blockchain import Blockchain
 from datetime import datetime
+from collections import Counter
 import json
 
 
@@ -30,10 +31,20 @@ def main():
     h = Hive(node='https://api.deathwing.me')
     blockchain = Blockchain()
     print('Watching...')
+    ids_custom_json = []
 
     stream = blockchain.stream(opNames=['custom_json'], raw_ops=False, threading=True, thread_num=4)
-
+    count = 0
+    start = datetime.utcnow()
     for post in stream:
+        id = post['id']
+        ids_custom_json.append(id)
+        count += 1
+        if (count % 1000 == 0):
+            t = datetime.utcnow() - start
+            print (f'Count: {count} - {t}')
+            print(json.dumps(Counter(ids_custom_json),indent=2))
+
         if post['id'] == 'hive-hydra':
             if  (set(post['required_posting_auths']) & set(allowed_accounts)):
                 data = json.loads(post.get('json'))
