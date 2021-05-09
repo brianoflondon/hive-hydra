deae2004dd877608c7dded52222de82953f1e429
Brian of London <bol@snnsite.com>
2021-05-09 10:53:46 +0300

    synced with podping.cloud rep

--- watcher_telegram.py
+++ watcher_telegram.py
@@ -40,10 +40,17 @@ if TELEGRAM_ALERTS:
 
 
 
-def get_allowed_accounts(acc_name) -> bool:
-    """ get a list of all accounts allowed to post by acc_name (podcastindex)
+def get_allowed_accounts(acc_name='podping') -> bool:
+    """ get a list of all accounts allowed to post by acc_name (podping)
         and only react to these accounts """
 
+    # Switching to a simpler authentication system. Only podpings from accounts which
+    # the PODPING Hive account FOLLOWS will be watched.
+    master_account = Account(acc_name, blockchain_instance=hive, lazy=True)
+    allowed = master_account.get_following()
+    return allowed
+
+    # Depreciated OLD method which was silly. Will remove later
     if USE_TEST_NODE:
         return ['learn-to-code','hive-hydra','hivehydra','flyingboy','blocktvnews']
 
@@ -75,6 +82,17 @@ def output(post) -> None:
     logging.info('Found alert: ' + data.get('url'))
     if TELEGRAM_ALERTS:
         telegram_q.put( (telegram_post, data) )
+
+def output_status(timestamp, pings, count_posts, time_to_now='', current_block_num='') -> None:
+    """ Writes out a status update at with some count data """
+    if time_to_now:
+        logging.info(f'{timestamp} PodPings: {pings} - Count: {count_posts} - Time Delta: {time_to_now}')
+
+    else:
+        logging.info(f'{timestamp} PodPings: {pings} - Count: {count_posts} - Current BlockNum: {current_block_num}')
+
+
+
 
 def telegram_post(data) -> None:
     """ Outputs to Telegram """
@@ -135,7 +153,7 @@ def scan_live(report_freq = None):
 
     if type(report_freq) == int:
         report_freq = timedelta(minutes=report_freq)
-    allowed_accounts = get_allowed_accounts('podcastindex')
+    allowed_accounts = get_allowed_accounts()
 
     blockchain = Blockchain(mode="head", blockchain_instance=hive)
     current_block_num = blockchain.get_current_block_num()
@@ -148,23 +166,27 @@ def scan_live(report_freq = None):
 
     start_time = datetime.utcnow()
     count_posts = 0
+    pings = 0
 
     for post in stream:
         count_posts +=1
         time_dif = post['timestamp'].replace(tzinfo=None) - start_time
         if time_dif > report_freq:
             current_block_num = blockchain.get_current_block_num()
-            logging.info(str(post['timestamp']) + " Count: " + str(count_posts) + " block_num: " + str(current_block_num))
+            timestamp = str(post['timestamp'])
+            output_status(timestamp, pings, count_posts, current_block_num=current_block_num)
             start_time =post['timestamp'].replace(tzinfo=None)
             count_posts = 0
+            pings = 0
 
         if allowed_op_id(post['id']):
             if  (set(post['required_posting_auths']) & set(allowed_accounts)):
                 output(post)
+                pings += 1
 
         if time_dif > timedelta(hours=1):
             # Refetch the allowed_accounts every hour in case we add one.
-            allowed_accounts = get_allowed_accounts('podcastindex')
+            allowed_accounts = get_allowed_accounts()
 
 def scan_history(timed= None, report_freq = None):
     """ Scans back in history timed time delta ago, reporting with report_freq
@@ -183,11 +205,12 @@ def scan_history(timed= None, report_freq = None):
     if type(report_freq) == int:
         report_freq = timedelta(minutes=report_freq)
 
-    allowed_accounts = get_allowed_accounts('podcastindex')
+    allowed_accounts = get_allowed_accounts()
 
     blockchain = Blockchain(mode="head", blockchain_instance=hive)
     start_time = datetime.utcnow() - timed
     count_posts = 0
+    pings = 0
     block_num = blockchain.get_estimated_block_num(start_time)
 
     logging.info('Started catching up')
@@ -200,9 +223,11 @@ def scan_history(timed= None, report_freq = None):
         time_to_now = datetime.utcnow() - post_time
         count_posts += 1
         if time_dif > report_freq:
-            logging.info(str(post['timestamp']) + " Count: " + str(count_posts) + " Time Delta: " + str(time_to_now))
+            timestamp = str(post['timestamp'])
+            output_status(timestamp, pings, count_posts, time_to_now)
             start_time =post['timestamp'].replace(tzinfo=None)
             count_posts = 0
+            pings = 0
 
         if allowed_op_id(post['id']):
             if  (set(post['required_posting_auths']) & set(allowed_accounts)):
